d10288a97dc2cce93fd9f6e3a3dfdcecfed5fe73
minato-jpn <minato567@outlook.jp>
2026-03-05 18:59:45 +0900

    微修正:時間の追加

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -1,9 +1,10 @@
 export const Message = () => {
     return (
-        <div className="border-2 shadow-xl p-4 w-1/3 rounded-b-lg rounded-tr-lg m-8">
-            <p>
+        <div className="border-2 shadow-xl p-4 w-1/3 max-w-1/3 rounded-b-lg rounded-tr-lg m-8 ">
+            <p className = "relative">
                 こんにちは、こちらはLINEのクローン作成のための遊び場です。
             </p>
+            <span className = "absolute left-64">00:00</span>
         </div>
     )
 }
